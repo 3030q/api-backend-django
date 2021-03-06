52f8ba1f6909e02514f0686293c29e62779f954d
3030q <nikita.rybin@ananas-web.ru>
2021-03-07 01:43:18 +0500

    fix

--- api_backend_parser/settings.py
+++ api_backend_parser/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'parser_project',
+    'parser_project'
 ]
 
 MIDDLEWARE = [
