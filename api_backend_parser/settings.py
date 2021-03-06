1a6ae5e1afcd784b4ea3133e17a4cbc81cbb84fc
3030q <nikita.rybin@ananas-web.ru>
2021-03-07 01:26:36 +0500

    fix

--- api_backend_parser/settings.py
+++ api_backend_parser/settings.py
@@ -21,7 +21,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get(".env.dev", "DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get(".env.dev",  "DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Quick-start development settings - unsuitable for production
