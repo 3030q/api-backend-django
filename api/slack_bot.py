904e6246d513cd51743e8139c6fb6ad9579a3b29
Nikita Rybin <ribin@tochka.com>
2021-06-21 00:23:27 +0500

    add slack_bot functionality

--- api/slack_bot.py
+++ api/slack_bot.py
@@ -7,10 +7,12 @@ from pathlib import Path
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from dotenv import load_dotenv
+from rest_framework.decorators import api_view
 
 from api_backend_parser import settings
 
-@csrf_exempt
+
+@api_view(['Post'])
 def hello(request):
     client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
     json_dict = json.loads(request.body.decode('utf-8'))
