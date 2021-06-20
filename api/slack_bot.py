8c890b1b7157e6a6200858280a1bfd21d050b26c
Nikita Rybin <ribin@tochka.com>
2021-06-21 02:00:12 +0500

    add slack_bot functionality

--- api/slack_bot.py
+++ api/slack_bot.py
@@ -14,19 +14,20 @@ from api_backend_parser import settings
 @api_view(['Post'])
 def hello(request):
     client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
-    json_dict = json.loads(request.body.decode('utf-8'))
-    with open('ccc.json', 'w') as file:
-        json.dump(json_dict, file)
-
-    if json_dict['token'] != settings.VERIFICATION_TOKEN:
+    data = request.data
+    if data['token'] != settings.VERIFICATION_TOKEN:
         return HttpResponse(status=403)
-    if 'type' in json_dict:
-        if json_dict['type'] == 'url_verification':
-            response_dict = {"challenge": json_dict['challenge']}
+    if 'type' in data:
+        if data['type'] == 'url_verification':
+            response_dict = {"challenge": data['challenge']}
             return JsonResponse(response_dict, safe=False)
-    if json_dict['command'] == '/hello_review_gator':
-        response_msg = ":wave:, Hello me"
+    if 'event' in data:
+        event_msg = data['event']
+        if ('subtype' in event_msg) and (event_msg['subtype'] == 'bot_message'):
+            return HttpResponse(status=200)
+    if data['command'] == '/hello_review_gator':
+        user = event_msg['user']
+        response_msg = ":wave:, Hello <@%s>" % user
         client.chat_postMessage(channel='#backend', text=response_msg)
-
         return HttpResponse(status=200)
     return HttpResponse(status=200)
