bde0d618bebbe6efafd42fbd06f135547304c795
Nikita Rybin <ribin@tochka.com>
2021-06-21 00:30:18 +0500

    add slack_bot functionality

--- api/slack_bot.py
+++ api/slack_bot.py
@@ -15,21 +15,5 @@ from api_backend_parser import settings
 @api_view(['Post'])
 def hello(request):
     client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
-    json_dict = json.loads(request.body.decode('utf-8'))
-    if json_dict['token'] != settings.VERIFICATION_TOKEN:
-        return HttpResponse(status=403)
-    if 'type' in json_dict:
-        if json_dict['type'] == 'url_verification':
-            response_dict = {"challenge": json_dict['challenge']}
-            return JsonResponse(response_dict, safe=False)
-    if 'event' in json_dict:
-        event_msg = json_dict['event']
-        if ('subtype' in event_msg) and (event_msg['subtype'] == 'bot_message'):
-            return HttpResponse(status=200)
-    if event_msg['type'] == 'message':
-        user = event_msg['user']
-        channel = event_msg['channel']
-        response_msg = ":wave:, Hello <@%s>" % user
-        client.chat_postMessage(channel='backend', text=response_msg)
-        return HttpResponse(status=200)
+    client.chat_postMessage(channel='backend', text='Titatutu')
     return HttpResponse(status=200)
